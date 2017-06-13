42f45290960b5af668697510045e067b727948b5
andrew.burn1ng <andrew.burn1ng@gmail.com>
2017-06-13 13:31:08 +0200

    initial

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.js
+
+/*
+ * Custom
+ */
+//= partials/counters.js
+//= partials/test1.js
